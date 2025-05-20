942aa45def4af9da8d57e09ffe8a68940e6426a8
Min Thant Oo <minthantoo.ardil@gmail.com>
2025-05-19 19:15:36 -0500

    Add PatientResponseDTO & PatientMapper

---
+++ patient-service/src/main/java/com/minthantoo/patient_service/repository/PatientRepository.java
@@ -0,0 +1,12 @@
+package com.minthantoo.patient_service.repository;
+
+import com.minthantoo.patient_service.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+
+}

0bfc846327d4f345a847f0d782b5d2c6187ed140
Min Thant Oo <minthantoo.ardil@gmail.com>
2025-05-24 17:00:06 -0500

    Add GET patient endpoint

---
+++ patient-service/src/main/java/com/minthantoo/patient_service/controller/PatientController.java
@@ -0,0 +1,48 @@
+package com.minthantoo.patient_service.controller;
+
+import com.minthantoo.patient_service.dto.PatientResponseDTO;
+import com.minthantoo.patient_service.service.PatientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+//  return a ResponseEntity type with a list of PatientResponseDTO
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- patient-service/src/main/java/com/minthantoo/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/minthantoo/patient_service/service/PatientService.java
@@ -1,6 +1,7 @@
 package com.minthantoo.patient_service.service;
 
 import com.minthantoo.patient_service.dto.PatientResponseDTO;
+import com.minthantoo.patient_service.mapper.PatientMapper;
 import com.minthantoo.patient_service.model.Patient;
 import com.minthantoo.patient_service.repository.PatientRepository;
 import org.springframework.stereotype.Service;
@@ -9,13 +10,23 @@ import java.util.List;
 
 @Service
 public class PatientService {
-    private PatientRepository patientRepository;
+    private final PatientRepository patientRepository;
 
     private PatientService(PatientRepository patientRepository) {
         this.patientRepository = patientRepository;
     }
 
-    public List<PatientResponseDTO> getPatients () {
+    public List<PatientResponseDTO> getPatients() {
         List<Patient> patients = patientRepository.findAll();
+
+//        List<PatientResponseDTO> patientResponseDTOs = patients.stream()
+//                .map(patient -> PatientMapper.toDTO(patient)).toList();
+
+//        List<PatientResponseDTO> patientResponseDTOs = patients.stream()
+//                .map(PatientMapper::toDTO).toList();
+//        return patientResponseDTOs;
+
+        return patients.stream()
+                .map(PatientMapper::toDTO).toList();
     }
 }

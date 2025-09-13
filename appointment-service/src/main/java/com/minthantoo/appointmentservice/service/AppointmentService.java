a551631ac5feb2c1079f1a5cb2cd6d9c7832c521
Min Thant Oo <minthantoo.ardil@gmail.com>
2025-09-13 15:50:35 -0500

    Got cached patient details and added it to dto

--- appointment-service/src/main/java/com/minthantoo/appointmentservice/service/AppointmentService.java
+++ appointment-service/src/main/java/com/minthantoo/appointmentservice/service/AppointmentService.java
@@ -1,7 +1,9 @@
 package com.minthantoo.appointmentservice.service;
 
 import com.minthantoo.appointmentservice.dto.AppointmentResponseDto;
+import com.minthantoo.appointmentservice.entity.CachedPatient;
 import com.minthantoo.appointmentservice.repository.AppointmentRepository;
+import com.minthantoo.appointmentservice.repository.CachedPatientRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -12,17 +14,26 @@ import java.util.List;
 public class AppointmentService {
 
     private final AppointmentRepository appointmentRepository;
+    private final CachedPatientRepository cachedPatientRepository;
 
-    public AppointmentService(AppointmentRepository appointmentRepository) {
+    public AppointmentService(AppointmentRepository appointmentRepository, CachedPatientRepository cachedPatientRepository) {
         this.appointmentRepository = appointmentRepository;
+        this.cachedPatientRepository = cachedPatientRepository;
     }
 
     public List<AppointmentResponseDto> getAppointmentsByDateRange(LocalDateTime from, LocalDateTime to) {
         return appointmentRepository.findByStartTimeBetween(from, to).stream()
                 .map(appointment -> {
+
+                    String name = cachedPatientRepository
+                            .findById(appointment.getPatientId())
+                            .map(CachedPatient::getFullName)
+                            .orElse("Unknown");
+
                     AppointmentResponseDto appointmentResponseDto = new AppointmentResponseDto();
                     appointmentResponseDto.setId(appointment.getId());
                     appointmentResponseDto.setPatientId(appointment.getPatientId());
+                    appointmentResponseDto.setPatientName(name);
                     appointmentResponseDto.setStartTime(appointment.getStartTime());
                     appointmentResponseDto.setEndTime(appointment.getEndTime());
                     appointmentResponseDto.setReason(appointment.getReason());

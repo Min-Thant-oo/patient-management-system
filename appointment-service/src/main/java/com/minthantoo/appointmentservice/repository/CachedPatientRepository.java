6fd9cb45969aea7f01acb61d27e919c1d3e03094
Min Thant Oo <minthantoo.ardil@gmail.com>
2025-09-13 14:26:16 -0500

    Added CachedPatientRepository IN AppointmentService

---
+++ appointment-service/src/main/java/com/minthantoo/appointmentservice/repository/CachedPatientRepository.java
@@ -0,0 +1,10 @@
+package com.minthantoo.appointmentservice.repository;
+
+import com.minthantoo.appointmentservice.entity.CachedPatient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CachedPatientRepository extends JpaRepository<CachedPatient, UUID> {
+
+}
--- appointment-service/src/main/java/com/minthantoo/appointmentservice/service/AppointmentService.java
+++ appointment-service/src/main/java/com/minthantoo/appointmentservice/service/AppointmentService.java
@@ -31,4 +31,6 @@ public class AppointmentService {
                     return appointmentResponseDto;
                 }).toList();
     }
+
+
 }
